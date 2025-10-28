8b098cdf5f7811b60a73128a8a76fea9bbc7ee9b
gadjodilo26 <contact@rms-dev.fr>
2025-10-28 11:35:40 +0100

    PremiumRepo

---
+++ assets/js/carousel-data.js
@@ -0,0 +1,307 @@
+// Données de carrousels générées automatiquement à partir du dossier assets/img-carroussel
+window.CAROUSEL_DATA = {
+  "AgencementExtérieur": {
+    "AbrisVélo": {
+      "date": "20240515",
+      "images": [
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-5.jpg",
+          "order": 5
+        },
+        {
+          "src": "assets/img-carroussel/AgencementExtérieur-AbrisVélo-20240515-6.jpg",
+          "order": 6
+        }
+      ]
+    }
+  },
+  "AgencementIntérieur": {
+    "Boutique&Bureau": {
+      "date": "20250313",
+      "images": [
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-5.jpg",
+          "order": 5
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-6.jpg",
+          "order": 6
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-7.jpg",
+          "order": 7
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-8.jpg",
+          "order": 8
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-9.jpg",
+          "order": 9
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-10.jpg",
+          "order": 10
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-11.jpg",
+          "order": 11
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-12.jpg",
+          "order": 12
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-13.jpg",
+          "order": 13
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-14.jpg",
+          "order": 14
+        },
+        {
+          "src": "assets/img-carroussel/AgencementIntérieur-Boutique&Bureau-20250313-15.jpg",
+          "order": 15
+        }
+      ]
+    }
+  },
+  "AménagementExtérieur": {
+    "CréationVoletPiscine": {
+      "date": "20240801",
+      "images": [
+        {
+          "src": "assets/img-carroussel/AménagementExtérieur-CréationVoletPiscine-20240801-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/AménagementExtérieur-CréationVoletPiscine-20240801-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/AménagementExtérieur-CréationVoletPiscine-20240801-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/AménagementExtérieur-CréationVoletPiscine-20240801-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/AménagementExtérieur-CréationVoletPiscine-20240801-5.jpg",
+          "order": 5
+        }
+      ]
+    }
+  },
+  "AménagementIntérieur": {
+    "AccueilJardinEnfant": {
+      "date": "20240126",
+      "images": [
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-AccueilJardinEnfant-20240126-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-AccueilJardinEnfant-20240126-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-AccueilJardinEnfant-20240126-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-AccueilJardinEnfant-20240126-4.jpg",
+          "order": 4
+        }
+      ]
+    },
+    "Démontage2StrucureHall": {
+      "date": "20250306",
+      "images": [
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-Démontage2StrucureHall-20250306-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-Démontage2StrucureHall-20250306-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-Démontage2StrucureHall-20250306-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-Démontage2StrucureHall-20250306-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/AménagementIntérieur-Démontage2StrucureHall-20250306-5.jpg",
+          "order": 5
+        }
+      ]
+    }
+  },
+  "Finition": {
+    "AprèsDémolition": {
+      "date": "20241125",
+      "images": [
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-5.jpg",
+          "order": 5
+        },
+        {
+          "src": "assets/img-carroussel/Finition-AprèsDémolition-20241125-6.jpg",
+          "order": 6
+        }
+      ]
+    }
+  },
+  "Finitions": {
+    "Parquet": {
+      "date": "20250605",
+      "images": [
+        {
+          "src": "assets/img-carroussel/Finitions-Parquet-20250605-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/Finitions-Parquet-20250605-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/Finitions-Parquet-20250605-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/Finitions-Parquet-20250605-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/Finitions-Parquet-20250605-5.jpg",
+          "order": 5
+        }
+      ]
+    }
+  },
+  "Menuiserie": {
+    "FabricationPorteStyleGrange": {
+      "date": "20250407",
+      "images": [
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-4.jpg",
+          "order": 4
+        },
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-5.jpg",
+          "order": 5
+        },
+        {
+          "src": "assets/img-carroussel/Menuiserie-FabricationPorteStyleGrange-20250407-6.jpg",
+          "order": 6
+        }
+      ]
+    }
+  },
+  "RestaurationMeuble": {
+    "Marqueterie": {
+      "date": "20221010",
+      "images": [
+        {
+          "src": "assets/img-carroussel/RestaurationMeuble-Marqueterie-20221010-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/RestaurationMeuble-Marqueterie-20221010-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/RestaurationMeuble-Marqueterie-20221010-3.jpg",
+          "order": 3
+        },
+        {
+          "src": "assets/img-carroussel/RestaurationMeuble-Marqueterie-20221010-4.jpg",
+          "order": 4
+        }
+      ]
+    }
+  },
+  "RénovationMobilier": {
+    "AérogommageArmoire": {
+      "date": "20250520",
+      "images": [
+        {
+          "src": "assets/img-carroussel/RénovationMobilier-AérogommageArmoire-20250520-1.jpg",
+          "order": 1
+        },
+        {
+          "src": "assets/img-carroussel/RénovationMobilier-AérogommageArmoire-20250520-2.jpg",
+          "order": 2
+        },
+        {
+          "src": "assets/img-carroussel/RénovationMobilier-AérogommageArmoire-20250520-3.jpg",
+          "order": 3
+        }
+      ]
+    }
+  }
+};
